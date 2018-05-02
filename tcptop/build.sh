4616b0363e3c2ca2b989efbed75e0344356178b4
zqy <850074816@qq.com>
2018-05-02 18:42:55 +0800

    update build.sh

--- tcptop/build.sh
+++ tcptop/build.sh
@@ -1,21 +1,28 @@
 
+#CXX="/usr/bin/c++"
+CXX="g++"
 
-CXX_FLAGS="-Wall  -isystem /usr/lib/llvm-3.7/include -O3 -DNDEBUG   -std=gnu++11"
+LLVM_PATH="/usr/lib/llvm-3.7"
+
+BCC_PATH="/home/zqy/WorkSpace/LabeledKernel/Reference/linux/eBPF/bcc"
+
+
+CXX_FLAGS="-Wall  -isystem ${LLVM_PATH}/include -O3 -DNDEBUG   -std=gnu++11"
 
 CXX_DEFINES= 
 
-CXX_INCLUDES="-I/usr/lib/llvm-3.7/include/../tools/clang/include -I/home/zqy/WorkSpace/LabeledKernel/Reference/linux/eBPF/bcc/src/cc -I/home/zqy/WorkSpace/LabeledKernel/Reference/linux/eBPF/bcc/src/cc/api "
+CXX_INCLUDES="-I${LLVM_PATH}/include/../tools/clang/include -I/home/zqy/WorkSpace/LabeledKernel/Reference/linux/eBPF/bcc/src/cc -I/home/zqy/WorkSpace/LabeledKernel/Reference/linux/eBPF/bcc/src/cc/api "
 
 
 
-LLVM_LIB="/usr/lib/llvm-3.7/lib/libclangFrontend.a /usr/lib/llvm-3.7/lib/libclangSerialization.a /usr/lib/llvm-3.7/lib/libclangDriver.a /usr/lib/llvm-3.7/lib/libclangParse.a /usr/lib/llvm-3.7/lib/libclangSema.a /usr/lib/llvm-3.7/lib/libclangCodeGen.a /usr/lib/llvm-3.7/lib/libclangAnalysis.a /usr/lib/llvm-3.7/lib/libclangRewrite.a /usr/lib/llvm-3.7/lib/libclangEdit.a /usr/lib/llvm-3.7/lib/libclangAST.a /usr/lib/llvm-3.7/lib/libclangLex.a /usr/lib/llvm-3.7/lib/libclangBasic.a /usr/lib/llvm-3.7/lib/libLLVMLTO.a /usr/lib/llvm-3.7/lib/libLLVMX86CodeGen.a /usr/lib/llvm-3.7/lib/libLLVMX86Desc.a /usr/lib/llvm-3.7/lib/libLLVMX86Info.a /usr/lib/llvm-3.7/lib/libLLVMMCDisassembler.a /usr/lib/llvm-3.7/lib/libLLVMX86AsmPrinter.a /usr/lib/llvm-3.7/lib/libLLVMX86Utils.a /usr/lib/llvm-3.7/lib/libLLVMPasses.a /usr/lib/llvm-3.7/lib/libLLVMipo.a /usr/lib/llvm-3.7/lib/libLLVMVectorize.a /usr/lib/llvm-3.7/lib/libLLVMOption.a /usr/lib/llvm-3.7/lib/libLLVMObjCARCOpts.a /usr/lib/llvm-3.7/lib/libLLVMMCJIT.a /usr/lib/llvm-3.7/lib/libLLVMExecutionEngine.a /usr/lib/llvm-3.7/lib/libLLVMRuntimeDyld.a /usr/lib/llvm-3.7/lib/libLLVMLinker.a /usr/lib/llvm-3.7/lib/libLLVMIRReader.a /usr/lib/llvm-3.7/lib/libLLVMAsmParser.a /usr/lib/llvm-3.7/lib/libLLVMDebugInfoDWARF.a /usr/lib/llvm-3.7/lib/libLLVMBPFCodeGen.a /usr/lib/llvm-3.7/lib/libLLVMSelectionDAG.a /usr/lib/llvm-3.7/lib/libLLVMBPFDesc.a /usr/lib/llvm-3.7/lib/libLLVMBPFInfo.a /usr/lib/llvm-3.7/lib/libLLVMBPFAsmPrinter.a /usr/lib/llvm-3.7/lib/libLLVMAsmPrinter.a /usr/lib/llvm-3.7/lib/libLLVMCodeGen.a /usr/lib/llvm-3.7/lib/libLLVMTarget.a /usr/lib/llvm-3.7/lib/libLLVMScalarOpts.a /usr/lib/llvm-3.7/lib/libLLVMProfileData.a /usr/lib/llvm-3.7/lib/libLLVMObject.a /usr/lib/llvm-3.7/lib/libLLVMMCParser.a /usr/lib/llvm-3.7/lib/libLLVMBitReader.a /usr/lib/llvm-3.7/lib/libLLVMInstCombine.a /usr/lib/llvm-3.7/lib/libLLVMInstrumentation.a /usr/lib/llvm-3.7/lib/libLLVMTransformUtils.a /usr/lib/llvm-3.7/lib/libLLVMipa.a /usr/lib/llvm-3.7/lib/libLLVMMC.a /usr/lib/llvm-3.7/lib/libLLVMAnalysis.a /usr/lib/llvm-3.7/lib/libLLVMBitWriter.a /usr/lib/llvm-3.7/lib/libLLVMCore.a /usr/lib/llvm-3.7/lib/libLLVMSupport.a"
+LLVM_LIB="${LLVM_PATH}/lib/libclangFrontend.a ${LLVM_PATH}/lib/libclangSerialization.a ${LLVM_PATH}/lib/libclangDriver.a ${LLVM_PATH}/lib/libclangParse.a ${LLVM_PATH}/lib/libclangSema.a ${LLVM_PATH}/lib/libclangCodeGen.a ${LLVM_PATH}/lib/libclangAnalysis.a ${LLVM_PATH}/lib/libclangRewrite.a ${LLVM_PATH}/lib/libclangEdit.a ${LLVM_PATH}/lib/libclangAST.a ${LLVM_PATH}/lib/libclangLex.a ${LLVM_PATH}/lib/libclangBasic.a ${LLVM_PATH}/lib/libLLVMLTO.a ${LLVM_PATH}/lib/libLLVMX86CodeGen.a ${LLVM_PATH}/lib/libLLVMX86Desc.a ${LLVM_PATH}/lib/libLLVMX86Info.a ${LLVM_PATH}/lib/libLLVMMCDisassembler.a ${LLVM_PATH}/lib/libLLVMX86AsmPrinter.a ${LLVM_PATH}/lib/libLLVMX86Utils.a ${LLVM_PATH}/lib/libLLVMPasses.a ${LLVM_PATH}/lib/libLLVMipo.a ${LLVM_PATH}/lib/libLLVMVectorize.a ${LLVM_PATH}/lib/libLLVMOption.a ${LLVM_PATH}/lib/libLLVMObjCARCOpts.a ${LLVM_PATH}/lib/libLLVMMCJIT.a ${LLVM_PATH}/lib/libLLVMExecutionEngine.a ${LLVM_PATH}/lib/libLLVMRuntimeDyld.a ${LLVM_PATH}/lib/libLLVMLinker.a ${LLVM_PATH}/lib/libLLVMIRReader.a ${LLVM_PATH}/lib/libLLVMAsmParser.a ${LLVM_PATH}/lib/libLLVMDebugInfoDWARF.a ${LLVM_PATH}/lib/libLLVMBPFCodeGen.a ${LLVM_PATH}/lib/libLLVMSelectionDAG.a ${LLVM_PATH}/lib/libLLVMBPFDesc.a ${LLVM_PATH}/lib/libLLVMBPFInfo.a ${LLVM_PATH}/lib/libLLVMBPFAsmPrinter.a ${LLVM_PATH}/lib/libLLVMAsmPrinter.a ${LLVM_PATH}/lib/libLLVMCodeGen.a ${LLVM_PATH}/lib/libLLVMTarget.a ${LLVM_PATH}/lib/libLLVMScalarOpts.a ${LLVM_PATH}/lib/libLLVMProfileData.a ${LLVM_PATH}/lib/libLLVMObject.a ${LLVM_PATH}/lib/libLLVMMCParser.a ${LLVM_PATH}/lib/libLLVMBitReader.a ${LLVM_PATH}/lib/libLLVMInstCombine.a ${LLVM_PATH}/lib/libLLVMInstrumentation.a ${LLVM_PATH}/lib/libLLVMTransformUtils.a ${LLVM_PATH}/lib/libLLVMipa.a ${LLVM_PATH}/lib/libLLVMMC.a ${LLVM_PATH}/lib/libLLVMAnalysis.a ${LLVM_PATH}/lib/libLLVMBitWriter.a ${LLVM_PATH}/lib/libLLVMCore.a ${LLVM_PATH}/lib/libLLVMSupport.a"
 
-BCC_PATH="/home/zqy/WorkSpace/LabeledKernel/Reference/linux/eBPF/bcc"
+
 
 
 
 /usr/bin/c++  $CXX_DEFINES $CXX_INCLUDES $CXX_FLAGS  -c *.cpp
 
 
-/usr/bin/c++   -Wall  -isystem /usr/lib/llvm-3.7/include -O3 -DNDEBUG  -rdynamic *.o  -o main $BCC_PATH/src/cc/libbcc.a $BCC_PATH/src/cc/frontends/b/libb_frontend.a $BCC_PATH/src/cc/frontends/clang/libclang_frontend.a $BCC_PATH/src/cc/libbpf.a  $LLVM_LIB -lz -lpthread -lffi -ledit -ltinfo -ldl -lm -lelf $BCC_PATH/src/cc/api/libapi-static.a $BCC_PATH/src/cc/usdt/libusdt-static.a $BCC_PATH/src/cc/libbcc-loader-static.a -lelf 
+/usr/bin/c++   -Wall  -isystem ${LLVM_PATH}/include -O3 -DNDEBUG  -rdynamic *.o  -o main ${BCC_PATH}/src/cc/libbcc.a ${BCC_PATH}/src/cc/frontends/b/libb_frontend.a ${BCC_PATH}/src/cc/frontends/clang/libclang_frontend.a ${BCC_PATH}/src/cc/libbpf.a  $LLVM_LIB -lz -lpthread -lffi -ledit -ltinfo -ldl -lm -lelf ${BCC_PATH}/src/cc/api/libapi-static.a ${BCC_PATH}/src/cc/usdt/libusdt-static.a ${BCC_PATH}/src/cc/libbcc-loader-static.a -lelf 
 
