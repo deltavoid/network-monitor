cd33a3414f69994c8467be25e5f3d2ef3024392a
zqy <850074816@qq.com>
2018-05-11 15:02:44 +0800

    A1 can run

--- lab/HelloWorld/A1.cc
+++ lab/HelloWorld/A1.cc
@@ -13,9 +13,9 @@
 const std::string BPF_PROGRAM = R"(
 #include <uapi/linux/ptrace.h>
 #include <bcc/proto.h>
-//#include <linux/skbuff.h>
-//#include <linux/netdevice.h>
-//#include <net/cls_cgroup.h>
+#include <linux/skbuff.h>
+#include <linux/netdevice.h>
+#include <net/cls_cgroup.h>
 
 struct net_dev_xmit_args
 {   uint64_t common__unused;
@@ -27,9 +27,30 @@ struct net_dev_xmit_args
 
 int on_net_dev_xmit(struct net_dev_xmit_args* args)
 {
-    bpf_trace_printk("Hello, World! Here I send a package!\n");
-    return 0;
+    struct sk_buff* skb = NULL;
+    struct net_device* dev = NULL;
+    struct sock* sk = NULL;
+    struct sock_cgroup_data* skcd = NULL;
+    u8 skcd_is_data = 0;
+    u32 skcd_classid = 0;
+    int len = 0;
+    u32 classid = 0;
+    char name[16] = {};
 
+    skb = args->skbaddr;
+    len = skb->len;
+    dev = skb->dev;
+    //name = dev->name;
+    bpf_probe_read_str((void*)name, sizeof(name), (void*)dev->name);
+
+    sk = skb->sk;
+    skcd_is_data = sk->sk_cgrp_data.is_data;
+    skcd_classid = sk->sk_cgrp_data.classid;
+    //classid = skcd_classid;
+    classid = (skcd_is_data & 1) ? skcd_classid : 0;
+
+    bpf_trace_printk("Hello, World! %s %d %d\n", name, classid, len);
+    return 0;
 }
 )";
 
